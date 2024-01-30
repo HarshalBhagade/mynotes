66403534011d5bd0386a735571ab136c12aa2159
HarshalBhagade <harshalbhagade12@gmail.com>
2024-01-30 23:26:10 +0530

    Initial commit

---
+++ lib/home page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class  home_page extends StatelessWidget {
+  const home_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('jod',style: TextStyle(color: Colors.red,),),
+      ),
+      body: Center(
+        child:
+        Text('aa gaya'),
+      ),
+    );
+  }
+}
